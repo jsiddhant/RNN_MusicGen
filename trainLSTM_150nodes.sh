f99c1b1f22328ae88fdff7269c6eedd077c09b65
Devendra Pratap Yadav <dpybros@gmail.com>
2019-02-27 12:09:59 -0800

    Added scripts to generate music and plots. Added trained model for 50, 150 nodes

---
+++ trainLSTM_150nodes.sh
@@ -0,0 +1,1 @@
+nohup python train_lstm.py -l 150 -m 150nodes -n 200 > runLogs/run_150nodes_log.txt 2>&1 </dev/null &
---
+++ trainLSTM_50nodes.sh
@@ -0,0 +1,1 @@
+nohup python train_lstm.py -l 50 -m 50nodes -n 200 > runLogs/run_50nodes_log.txt 2>&1 </dev/null &
